d7f3c2004076be308e412018458de7354f6207f8
Andrea Valdecantos <andreavaldecantostorres@gmail.com>
2023-05-18 18:44:42 -0300

    comentado

--- practico6/Program.cs
+++ practico6/Program.cs
@@ -7,30 +7,31 @@
 // Console.WriteLine("valor de b:"+b);
 
 //EJERICIO 1
-string numeroAInvertir;
-int numero = 0;
-string numeroInvertido = "";
-Console.WriteLine("Ingrese un número");
-numeroAInvertir = Console.ReadLine();
+// string numeroAInvertir;
+// int numero = 0;
+// string numeroInvertido = "";
+// Console.WriteLine("Ingrese un número");
+// numeroAInvertir = Console.ReadLine();
 
-if (int.TryParse(numeroAInvertir, out numero))
-{
-    if (numero > 0)
-    {
-        while (numero > 1)
-        {
-            numeroInvertido = numeroInvertido + (numero % 10);
-            numero = numero/10;
-        }
-    }
-} else {
-    Console.WriteLine("El número no es válido");
-}
+// if (int.TryParse(numeroAInvertir, out numero))
+// {
+//     if (numero > 0)
+//     {
+//         while (numero > 1)
+//         {
+//             numeroInvertido = numeroInvertido + (numero % 10);
+//             numero = numero/10;
+//         }
+//     }
+// } else {
+//     Console.WriteLine("El número no es válido");
+// }
 
-Console.WriteLine(numeroInvertido);
+// Console.WriteLine(numeroInvertido);
 
-
+//no va
 // if (int.TryParse(numeroInvertido, out numero))
 // {
 //     Console.WriteLine(numero);
 // }
+
