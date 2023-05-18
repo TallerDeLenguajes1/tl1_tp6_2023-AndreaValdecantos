ced7899382ba8212a76cce54ff794621209499bd
Andrea Valdecantos <andreavaldecantostorres@gmail.com>
2023-05-18 18:39:24 -0300

    ejercicio1

---
+++ practico6/Program.cs
@@ -0,0 +1,35 @@
+﻿// Console.WriteLine("Hello, World!");
+// int a;
+// int b;
+// a=10;
+// b=a;
+// Console.WriteLine("valor de a:"+a);
+// Console.WriteLine("valor de b:"+b);
+
+string numeroAInvertir;
+int numero = 0;
+string numeroInvertido = "";
+Console.WriteLine("Ingrese un número");
+numeroAInvertir = Console.ReadLine();
+
+if (int.TryParse(numeroAInvertir, out numero))
+{
+    if (numero > 0)
+    {
+        while (numero > 1)
+        {
+            numeroInvertido = numeroInvertido + (numero % 10);
+            numero = numero/10;
+        }
+    }
+} else {
+    Console.WriteLine("El número no es válido");
+}
+
+Console.WriteLine(numeroInvertido);
+
+
+// if (int.TryParse(numeroInvertido, out numero))
+// {
+//     Console.WriteLine(numero);
+// }
